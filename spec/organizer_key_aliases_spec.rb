4d023ec5b5169db896c8f3d57d5d2173f656dd6d
Ben Schmeckpeper <ben.schmeckpeper@gmail.com>
2016-10-17 23:30:08 -0500

    Aliases can mask typos in promised/expected keys

--- spec/organizer_key_aliases_spec.rb
+++ spec/organizer_key_aliases_spec.rb
@@ -51,4 +51,30 @@ describe "organizer aliases macro" do
       expect(result.expected_key).to eq(result[:promised_key])
     end
   end
+
+  context "when an aliased key is typo'd" do
+    let(:organizer_with_alias) do
+      Class.new do
+        extend LightService::Organizer
+
+        aliases :promiser_key => :expected_key
+
+        def self.do_something(ctx = {})
+          with(ctx).reduce(
+            [
+              TestDoubles::PromisesPromisedKeyAction,
+              TestDoubles::ExpectsExpectedKeyAction
+            ]
+          )
+        end
+      end
+    end
+
+    it "raises an ExpectedKeysNotInContextError" do
+      # This is a non-obvious test.  The Organizer class is aliasing promiser_key to expected_key, but the
+      # PromisesPromisedKeyAction is setting promised_key in the context.  We should catch that expected_key
+      # dereferences to a non-existent key.
+      expect(organizer_with_alias.do_something).to raise_error(LightService::ExpectedKeysNotInContextError)
+    end
+  end
 end
